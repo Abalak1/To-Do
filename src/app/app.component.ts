235449351af4c10bae26fddd16c76966e3556955
FIRST_NAME LAST_NAME <abalaki03@gmail.com>
2022-03-16 00:20:43 +0400

    logic created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Todo List';
+  toDoList:any[]=[]
+  inProgressList:any[]=[]
+  done:any[]=[]
+
+  addTask(name:string,selectedOption:string){
+    
+    this.toDoList.push({id:this.toDoList.length,name,selectedOption})
+  }
+  removeTask(id:number){
+
+    this.toDoList=this.toDoList.filter(item=>item.id!==id)
+  }
+  moveTask(id:number){
+    
+    const item = this.toDoList.find((e)=>e.id==id)
+    this.inProgressList.push({...item,id:this.toDoList.length})
+    this.toDoList=this.toDoList.filter((e)=>e.id!==id)
+
+
+  }
+  moveTaskDone(id:number){
+    
+    const item = this.inProgressList.find((e)=>e.id==id)
+    this.done.push({...item,id:this.done.length,})
+    this.inProgressList=this.inProgressList.filter((e)=>e.id!==id)
+
+  }
+  moveTaskBackProgress(id:number,){
+    
+    const item = this.done.find((e)=>e.id==id)
+    this.inProgressList.push({...item,id:this.inProgressList.length})
+    this.done=this.done.filter((e)=>e.id!==id)
+
+  }
+
+  moveTaskBackToDo(id:number){
+    
+    const item = this.inProgressList.find((e)=>e.id==id)
+    this.toDoList.push({...item,id:this.toDoList.length})
+    this.inProgressList=this.inProgressList.filter((e)=>e.id!==id)
+
+  }
+
+  selectedOption: any;
+  printedOption: any;
+
+  
+  print() {
+    this.printedOption = this.selectedOption;
+
+  }
+  
+ 
 }
